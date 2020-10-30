1ef82a0de3fe0b8b88b0d3e46d79f88679c131cb
swonbiz <chs071368@naver.com>
2020-10-31 05:59:59 +0900

    Apply Material-ui

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
 import React, { Component } from 'react';
 import Customer from './component/Customer' ;
+import Paper from '@material-ui/core/Paper'
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import {withStyles} from '@material-ui/core/styles' ;
+
+const styles = theme => ({
+  root:{
+    width:'100%',
+    marginTop: theme.spacing.unit *3,
+    overflowx:"auto"
+  },
+  table:{
+    minWidth:1080
+  }
+})
 
 const customers=[
   {
@@ -54,8 +72,29 @@ const customers=[
 
 class App extends Component {
   render(){
+    const {classes } = this.props;
     return (
-      <div >
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>상호</TableCell>
+              <TableCell>사업자번호</TableCell>
+              <TableCell>대표자</TableCell>
+              <TableCell>대표자HP</TableCell>
+              <TableCell>대표자 E-mail</TableCell>
+              <TableCell>회사구분</TableCell>
+              <TableCell>계약구분</TableCell>
+              <TableCell>대표자주소</TableCell>
+              <TableCell>계약기간</TableCell>
+              <TableCell>계약기간</TableCell>
+              <TableCell>입금일자</TableCell>
+              <TableCell>월회비</TableCell>
+              <TableCell>이미지</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
         {
           customers.map(c =>{
             return(
@@ -79,10 +118,12 @@ class App extends Component {
             );
           })
         }
-      </div>
+          </TableBody>
+        </Table>
+      </Paper>
     );
   }
   
 }
 
-export default App;
+export default withStyles(styles)(App);
--- src/component/Customer.js
+++ src/component/Customer.js
@@ -1,24 +1,26 @@
 import React, { Component } from 'react' ;
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
 
 class Customer extends Component{
     render(){
         return(
-            <div>
-                <h2>{this.props.id}</h2>
-                 <p>{this.props.company_name}</p>
-                 <p>{this.props.company_reg_no}</p>
-                 <p>{this.props.company_ceo}</p>
-                 <p>{this.props.company_ceo_phone}</p>
-                 <p>{this.props.company_ceo_email}</p>
-                 <p>{this.props.company_tp}</p>
-                 <p>{this.props.contract_tp}</p>
-                 <p>{this.props.company_ceo_address}</p>
-                 <p>{this.props.contract_start_end_date}</p>
-                 <p>{this.props.contract_term}</p>
-                 <p>{this.props.imcome_date}</p>
-                 <p>{this.props.imcome_fee}</p>
-                 <p>{this.props.image}</p>
-            </div>
+            <TableRow>
+                <TableCell>{this.props.id}</TableCell>
+                 <TableCell>{this.props.company_name}</TableCell>
+                 <TableCell>{this.props.company_reg_no}</TableCell>
+                 <TableCell>{this.props.company_ceo}</TableCell>
+                 <TableCell>{this.props.company_ceo_phone}</TableCell>
+                 <TableCell>{this.props.company_ceo_email}</TableCell>
+                 <TableCell>{this.props.company_tp}</TableCell>
+                 <TableCell>{this.props.contract_tp}</TableCell>
+                 <TableCell>{this.props.company_ceo_address}</TableCell>
+                 <TableCell>{this.props.contract_start_end_date}</TableCell>
+                 <TableCell>{this.props.contract_term}</TableCell>
+                 <TableCell>{this.props.imcome_date}</TableCell>
+                 <TableCell>{this.props.imcome_fee}</TableCell>
+                 <TableCell>{this.props.image}</TableCell>
+            </TableRow>
         )
     }
 
