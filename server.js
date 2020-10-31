1d5e3cab1249a8dc77c23d7996dd45e2c30054a6
swonbiz <chs071368@naver.com>
2020-10-31 13:11:07 +0900

    create customr database table & setting

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express= require('express') ;
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,63 +7,27 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
-app.get('/api/hello', (req, res)=>{
-    res.send({message:'Hello Express'});
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql= require('mysql');
+
+const connection = mysql.createConnection({
+    host:conf.host,
+    user:conf.user,
+    password:conf.password,
+    port:conf.port,
+    database:conf.database
 });
+connection.connect();
+
+// app.get('/api/hello', (req, res)=>{
+//     res.send({message:'Hello Express'});
+// });
 
 app.get('/api/customers', (req, res)=>{
-    res.send([
-        {
-        'id':1,
-        'image':'https://placeimg.com/64/64/1',
-        'company_name':'(주)와인드엘',
-        'company_reg_no':'804-81-01720',
-        'company_ceo':'박새롬',
-        'company_ceo_phone':'010-6403-8449',
-        'company_ceo_email':'gksvlal227@gmail.com',
-        'company_tp':'법인',
-        'contract_tp':'Flex',
-        'company_ceo_address':'서울 중구 흥인동 142-1 101동 1202호',
-        'contract_start_end_date':'2020.07.02 ~ 2021.01.01',
-        'contract_term':'6개월',
-        'imcome_date':'매월2일',
-        'imcome_fee':'198,000'
-      },
-      {
-        'id':2,
-        'image':'https://placeimg.com/64/64/2',
-        'company_name':'(주)에스원테크',
-        'company_reg_no':'105-87-68698',
-        'company_ceo':'최현수',
-        'company_ceo_phone':'010-6484-7287',
-        'company_ceo_email':'hschoi@s-onetech.com',
-        'company_tp':'법인',
-        'contract_tp':'Flex',
-        'company_ceo_address':'서울 노원구 월계동 그랑빌아파트 121-303',
-        'contract_start_end_date':'2020.01.02 ~ 2021.01.01',
-        'contract_term':'12개월',
-        'imcome_date':'매월2일',
-        'imcome_fee':'198,000'
-      },
-      {
-        'id':3,
-        'image':'https://placeimg.com/64/64/3',
-        'company_name':'에스원3DP',
-        'company_reg_no':'237-39-00131',
-        'company_ceo':'최현수',
-        'company_ceo_phone':'010-6484-7287',
-        'company_ceo_email':'chs071368@naver.com',
-        'company_tp':'개인',
-        'contract_tp':'비상주',
-        'company_ceo_address':'서울 노원구 월계동 그랑빌아파트 121-303',
-        'contract_start_end_date':'2019.01.02 ~ 2021.01.01',
-        'contract_term':'24개월',
-        'imcome_date':'매월2일',
-        'imcome_fee':'198,000'
-      }
-      ]
-        
-    );
+    connection.query("SELECT * FROM CUSTOMER", 
+    (err, rows, fields) =>{res.send(rows);
+    });
 });
 
 app.listen(port, ()=>console.log(`Listening on port ${port}`));
